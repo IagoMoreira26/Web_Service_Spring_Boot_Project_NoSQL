b9a84cb503b8695ce0c7621b1a7ef1bab9f75977
IagoMoreira26 <iagomoreira2353@gmail.com>
2026-01-12 18:43:58 -0300

    Connecting mongoDB to repository and service

--- src/main/java/com/iagomoreira/workshopmongo/domain/User.java
+++ src/main/java/com/iagomoreira/workshopmongo/domain/User.java
@@ -1,57 +1,64 @@
 package com.iagomoreira.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection = "user")
 public class User implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private String id;
-    private String name;
-    private String email;
+	private static final long serialVersionUID = 1L;
 
-    public User() {
-    }
+	@Id
+	private String id;
+	private String name;
+	private String email;
 
-    public User(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
+	public User() {
+	}
 
-    public String getId() {
-        return id;
-    }
+	public User(String id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass())
+			return false;
+		User user = (User) o;
+		return Objects.equals(id, user.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
 }
--- src/main/java/com/iagomoreira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/iagomoreira/workshopmongo/resources/UserResource.java
@@ -1,25 +1,26 @@
 package com.iagomoreira.workshopmongo.resources;
 
 import com.iagomoreira.workshopmongo.domain.User;
+import com.iagomoreira.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "Maria@gmail.com");
-        User alex = new User("2", "Alex Green", "Alex@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
-        return ResponseEntity.ok().body(list);
-    }
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 }
