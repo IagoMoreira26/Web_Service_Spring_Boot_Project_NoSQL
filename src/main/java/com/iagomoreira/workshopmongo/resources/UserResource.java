2c234f6d4ed51e5455ede3cac6180dba86cdc34a
IagoMoreira26 <iagomoreira2353@gmail.com>
2026-01-24 21:11:24 -0300

    Endpoint for a user's posts

--- src/main/java/com/iagomoreira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/iagomoreira/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.iagomoreira.workshopmongo.domain.Post;
 import com.iagomoreira.workshopmongo.domain.User;
 import com.iagomoreira.workshopmongo.dto.UserDTO;
 import com.iagomoreira.workshopmongo.services.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		user.setId(id);
 		user = userService.update(user);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
